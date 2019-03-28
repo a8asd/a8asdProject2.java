a704e0dc88cec6423c4f11c216764b1e36e0edb7
davidputman <davidputman@davidputman.com>
2019-03-28 15:33:51 +0000

    reset

--- examples/src/test/java/com/agil8/examples/bdd/ArgumentOutOfRangeException.java
+++
@@ -1,9 +0,0 @@
-package com.agil8.examples.bdd;
-
-public class ArgumentOutOfRangeException extends RuntimeException {
-
-	public ArgumentOutOfRangeException(String string) {
-		super(string);
-	}
-
-}
--- luber/src/main/java/com/agil8/luber/LuberApp.java
+++ luber/src/main/java/com/agil8/luber/LuberApp.java
@@ -26,6 +26,5 @@ public class LuberApp {
 				return;
 			}
 		}
-	
 	}
 }
