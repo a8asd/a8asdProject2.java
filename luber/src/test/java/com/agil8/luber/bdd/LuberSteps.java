a79405ce98111114bc39a57ab4b6b45ae5ed9235
david putman <davidputman@davidputman.com>
2019-05-26 17:37:37 +0100

    practice makes perfect

--- luber/src/test/java/com/agil8/luber/bdd/LuberSteps.java
+++ luber/src/test/java/com/agil8/luber/bdd/LuberSteps.java
@@ -1,49 +1,4 @@
 package com.agil8.luber.bdd;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-
-import com.agil8.luber.Driver;
-import com.agil8.luber.LuberApp;
-
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
 public class LuberSteps {
-	LuberApp context = new LuberApp();
-
-	@Given("^(.*) is a driver$")
-	public void a_driver(String driverName) {
-		Driver driver = new Driver(driverName, true);
-		context.addDriver(driver);
-	}
-
-	public class DriverData {
-		public String name;
-		public boolean available;
-	}
-
-	@Given("^these drivers$")
-	public void these_drivers(List<DriverData> driverTable) {
-		for (DriverData driverdata : driverTable) {
-			context.addDriver(new Driver(driverdata.name, driverdata.available));
-		}
-	}
-
-	@When("^(.*) books a ride with (.*)$")
-	public void client_books_a_ride_with_driver(String client, String driver) {
-		context.addBookingToDriver(client, driver);
-	}
-
-	@When("^(.*) books first available driver$")
-	public void client_books_first_available_driver(String client) {
-		context.bookFirstAvailableDriver(client);
-	}
-
-	@Then("^(.*) receives (.*)'s booking$")
-	public void driver_receives_the_client_booking(String driver, String client) {
-		assertTrue(context.getDriverBookings(driver).contains(client));
-	}
 }
--- luber/src/test/resources/com/agil8/luber/bdd/bookride.feature
+++ luber/src/test/resources/com/agil8/luber/bdd/bookride.feature
@@ -1,24 +1,9 @@
 Feature: book a ride
 
   Scenario: book a ride
-    Given Steve is a driver
-    When John books a ride with Steve
-    Then Steve receives John's booking
-
-  Scenario Outline: book a ride
-    Given <driver> is a driver
-    When <client> books a ride with <driver>
-    Then <driver> receives <client>'s booking
-
-    Examples: 
-      | driver | client |
-      | brian  | steve  |
-      | peter  | george |
-
-  Scenario: book first available driver
-    Given these drivers
-      | name  | available |
-      | Brian | false     |
-      | Peter | true      |
-    When John books first available driver
-    Then Peter receives John's booking
+    Given david is a customer
+    When david request a ride
+    Then these drivers are available:
+      | driver |
+      | fred   |
+      | pete   |
